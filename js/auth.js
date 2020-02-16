06166e9089df435efbd21ae400d982cc1a75b603
axiotidis <50782505+axiotidis@users.noreply.github.com>
2020-02-16 18:58:32 +0200

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,7 @@
+var email= axiotidis@sch.gr;
+var password = nki3956;
+// Sign in existing user
+firebase.auth().signInWithEmailAndPassword(email, password)
+ .catch(function(err) {
+   alert("Login failed");
+ });
