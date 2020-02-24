7a5c7b7c18e02b2652187554c6bfa57c041401f8
axiotidis <50782505+axiotidis@users.noreply.github.com>
2020-02-25 00:49:54 +0200

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,6 +1,6 @@
 var email= "axiotidis@sch.gr";
 var password = "nki3956";
-// Sign in existing user
+// Forced Sign in existing user
 firebase.auth().signInWithEmailAndPassword(email, password)
  .catch(function(err) {
    alert("Login failed");
